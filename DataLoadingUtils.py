bffd9cba320a5e2adf24c11671d235f58084cf38
Maneesh Gupta <gmanees@amazon.com>
2022-01-09 15:24:57 +0530

    Working VGG16 Model, code cleanup

--- DataLoadingUtils.py
+++ DataLoadingUtils.py
@@ -1,32 +1,28 @@
-import random
-import numpy as np
-import cv2
-import tensorflow as tf
-import os
-from PIL import Image
+import inspect
 import json
+import os
+import random
+
+import numpy as np
+import tensorflow as tf
 from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
-from keras.preprocessing.image import load_img, img_to_array
 
-# custom augmentation functions
-#from image_augmentation import *
 from common.Constants import *
-npix = 224
-target_size = (npix,npix,3)
 
-def default_loader(path):
+model_dictionary = {m[0]:m[1] for m in inspect.getmembers(tf.keras.applications, inspect.ismodule)}
+
+def default_loader(path,modelName):
     img = image.load_img(path, target_size=(224, 224))
     x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    #x = np.expand_dims(x, axis=0)
+    x = model_dictionary[modelName.lower()].preprocess_input(x)
     return x
+
 
 #### tensorflow data generator
 class TensorflowDataGenerator(tf.keras.utils.Sequence):  #
-    def __init__(self, root, filenameList, batch_size, im_size=224, shuffle=True, transform=None,
+    def __init__(self, root, filenameList, modelName, batch_size, im_size=224, shuffle=True, transform=None,
                  target_transform=None, loader=default_loader):
-        print("TensorflowDataGenerator: __init__")
         datafile = open(filenameList, "r")
         content_list = datafile.read().splitlines()
 
@@ -35,6 +31,7 @@ class TensorflowDataGenerator(tf.keras.utils.Sequence):  #
         self.imagesList = content_list
 
         self.root = root
+        self.modelName = modelName
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.on_epoch_end()
@@ -44,77 +41,34 @@ class TensorflowDataGenerator(tf.keras.utils.Sequence):  #
         self.loader = loader
         self.ImageCount = len(self.imagesList)
 
-
     def on_epoch_end(self):
-        print("TensorflowDataGenerator: on_epoch_end")
         if self.shuffle == True:
             random.shuffle(self.imagesList)
 
     def __len__(self):
-        print("TensorflowDataGenerator: __len__")
         # Denotes the number of batches per epoch
-        return len(self.imagesList) #// self.batch_size
-
+        return len(self.imagesList) // self.batch_size
 
     def __getitem__(self, index):
-        print("TensorflowDataGenerator: __getitem__ ")
-        # pick up the image
-        filename = self.imagesList[index]
-        metaFilename = os.path.join(METADATA_DIR, ('%s.json' % filename))
-        imageFilename = os.path.join(IMAGE_DIR, ('%s.jpg' % filename))
-
-        img = self.loader(imageFilename)
-        #if self.transform is not None:
-        #    img = self.transform(img)
-
-        metadata = json.loads(open(metaFilename).read())
-        quantity = metadata['EXPECTED_QUANTITY']
-#        if self.target_transform is not None:
-#            target = self.target_transform(target)
-        return img, np.array([quantity])
-"""
         # Generate one batch of data
         # Generate indices of the batch
-        batch = self.train_imgs[index * self.batch_size:(index + 1) * self.batch_size]
+        batch = self.imagesList[index * self.batch_size:(index + 1) * self.batch_size]
         X, y = self.__get_data(batch)
         return X, y
-"""
 
-"""
     def __get_data(self, batch):
-        print("TensorflowDataGenerator:__get_data ")
-        return
-
         X = []
         y = []
-        for file_name in batch:
-            im = cv2.imread(file_name, cv2.IMREAD_COLOR)
-            im = augment(im, im_size=self.im_size,
-                         random_crop_flag=1,
-                         resize_flag=1)
-            X.append(im)
-            if 'dog' in file_name:
-                y.append(1)
-            elif 'cat' in file_name:
-                y.append(0)
-        return np.asarray(X), np.asarray(y).astype('float32').reshape((-1, 1))
-"""
+        for filename in batch:
+            metaFilename = os.path.join(METADATA_DIR, ('%s.json' % filename))
+            imageFilename = os.path.join(IMAGE_DIR, ('%s.jpg' % filename))
 
-"""
-    def load_val(self):
-        print("TensorflowDataGenerator: load_val")
+            img = self.loader(imageFilename, self.modelName)
 
-        X = []
-        y = []
-        for file_name in self.val_imgs:
-            im = cv2.imread(file_name, cv2.IMREAD_COLOR)
-            im = augment(im, self.im_size,
-                         random_crop_flag=0,
-                         resize_flag=1)
-            X.append(im)
-            if 'dog' in file_name:
-                y.append(1)
-            elif 'cat' in file_name:
-                y.append(0)
-        return (np.asarray(X), np.asarray(y).astype('float32').reshape((-1, 1)))
-"""
+            X.append(img)
+
+            metadata = json.loads(open(metaFilename).read())
+            quantity = metadata['EXPECTED_QUANTITY']
+
+            y.append(quantity)
+        return np.asarray(X), np.asarray(y).astype('float32').reshape((-1,1))
--- main.py
+++ main.py
@@ -2,31 +2,34 @@
 import argparse
 import inspect
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import matplotlib.pyplot as plt
-
+import keras
 from DataLoadingUtils import TensorflowDataGenerator
-import tensorflow_datasets as tfds
-import pandas as pd
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-
-from concurrent.futures import ThreadPoolExecutor
-
 from common.ImageDataSetSplit import *
-from ImageModule.CNNModule import *
-from ImageModule.ResnetModule import *
-from ImageModule.SiasmeVGGModule import *
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 model_dictionary = {m[0]:m[1] for m in inspect.getmembers(tf.keras.applications, inspect.isfunction)}
 model_names = model_dictionary.keys()
 
 parser = argparse.ArgumentParser(description='Image Object Counting Model')
-parser.add_argument('--model', '-m', metavar='MODEL', default='VGG16', choices=model_names, help='model architecture: ' + ' | '.join(model_names) + ' (default: VGG16)')
+parser.add_argument('--model', '-m', metavar='MODEL', default='ResNet50', choices=model_names, help='model architecture: ' + ' | '.join(model_names) + ' (default: ResNet50)')
 parser.add_argument('-j', '--workers', default=8, type=int, metavar='N', help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=30, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--epochs', default=3, type=int, metavar='N', help='number of total epochs to run')
 parser.add_argument('-b', '--batch_size', default=128, type=int, metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, metavar='LR', help='initial learning rate')
 parser.add_argument('--lrd','--learning-rate-decay-step', default=10, type=int, metavar='N', help='learning rate decay epoch')
@@ -37,26 +40,6 @@ parser.add_argument('--print-freq', '-p', default=10, type=int, metavar='N', hel
 parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('--evaluate', default=False, type=bool, metavar='BOOL', help='evaluate or train')
 
-def compareAllAlgo():
-    print("compareAllAlgo -->")
-    countWithCNN()
-    countWithCNNAndPreprocess()
-    countWithResNet()
-    countWithResNetAndPreprocess()
-    countWithSiasme()
-    countWithSiasmeAndPreprocess()
-
-def show_choices():
-    print('\nMenu')
-    print('1. Count with CNN')
-    print('2. Count with ResNet')
-    print('3. Count with Siasme')
-    print('4. Count with CNN and PreProcess')
-    print('5. Count with ResNet and PreProcess')
-    print('6. Count with Siasme and PreProcess')
-    print('7. Print Comparison')
-    print('p. Preprocess Input Images')
-    print('X. Exit')
 
 def oneTimeImageProcssing():
     # Resize all images to 224x244, One time operation, so commenting call
@@ -73,62 +56,30 @@ def mainMenu():
 
     # create model
     print("=> creating model '{}'".format(args.model))
-
     model_func = model_dictionary[args.model]
+
     print("Building Model")
     model = model_func()
 
-    model.compile('adam', loss='mse')
-    prepareTrainValidateTestSplitDataset(0.7,0.1,0.2)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=args.lr,
+        decay_steps=10000,
+        decay_rate=args.lrd)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=args.momentum)
 
-    training_generator = TensorflowDataGenerator(IMAGE_DIR,TRAIN_SET_FILENAME,32)
-    validation_generator = TensorflowDataGenerator(IMAGE_DIR,VALIDATION_SET_FILENAME,32)
+    model.compile(loss='mse',optimizer=optimizer, metrics=['accuracy', 'mse'])
 
-    model.fit_generator(generator=training_generator,
-                    validation_data=validation_generator,
-                    use_multiprocessing=True,
-                    workers=12)
+#    prepareTrainValidateTestSplitDataset(0.7,0.1,0.2)
 
-    """
-    trainData = readTrainData()
-    validationData = readValidationData()
-    testData = readTestData()
-    print ("Train Data", trainData)
-    print("Validation Data", validationData)
-    print("testData", testData)
+    training_generator = TensorflowDataGenerator(IMAGE_DIR,TRAIN_SET_FILENAME,args.model,args.batch_size)
+    validation_generator = TensorflowDataGenerator(IMAGE_DIR,VALIDATION_SET_FILENAME,args.model,args.batch_size)
 
-
-    while(True):
-        show_choices()
-        choice = input('Enter choice(1-7,p,X): ')
-        if choice == '1':
-            countWithCNN()
-            print("Count with CNN")
-        elif choice == '2':
-            countWithResNet()
-            print("Count with ResNet")
-        elif choice == '3':
-            countWithSiasme()
-            print("Count with Siasme")
-        elif choice == '4':
-            countWithCNNAndPreprocess()
-            print("Count with CNN and PreProcess")
-        elif choice == '5':
-            countWithResNetAndPreprocess()
-            print("Count with ResNet and PreProcess")
-        elif choice == '6':
-            countWithSiasmeAndPreprocess()
-            print("Count with Siasme and PreProcess")
-        elif choice == '7':
-            compareAllAlgo()
-            print("Show Comparison")
-        elif choice == 'p':
-            oneTimeImageProcessing()
-            print("Preprocess Images")
-        else:
-            print('Invalid input')
-            break
-"""
+    model.fit(x=training_generator,
+                validation_data=validation_generator,
+                epochs=args.epochs,
+                verbose=1,
+                use_multiprocessing=True,
+                workers=12)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
