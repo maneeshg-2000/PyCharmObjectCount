877f88657580b6863beb9f366fa30712b5f40e20
Maneesh Gupta <gmanees@amazon.com>
2022-01-13 22:47:09 +0530

    Updated Model with Output layer fixed

--- common/ImageDataSetSplit.py
+++ common/ImageDataSetSplit.py
@@ -19,61 +19,48 @@ ClassTestFileName = [ TEST_SET_CLASS_BASED_FILENAME + "_Zero" + ".txt",
                  TEST_SET_CLASS_BASED_FILENAME + "_Four" + ".txt",
                  TEST_SET_CLASS_BASED_FILENAME + "_Five" + ".txt",
                  TEST_SET_CLASS_BASED_FILENAME + "_Six" + ".txt"]
-def plthist(dfsub, topn=-1):
-    plt.figure(figsize=(5,2))
-    plt.xlabel('Quantity in a bin image')
-    plt.ylabel('The number of bin images')
-    #plt.bar(dfsub.index, dfsub["Quantity"])
-    plt.plot(dfsub[:topn])
-    plt.yticks(fontsize=12)
-    #plt.xticks(dfsub.index,dfsub["word"],rotation=90,fontsize=20)
-    plt.title("Bin Item Count Distribution",fontsize=12)
-    plt.show()
-
 
 def dataVisualization():
+
     imageList = os.listdir(IMAGE_DIR)
     imageCount = len(imageList)
-    quantity_hist = np.zeros(100, dtype=int)
+    #quantity_hist = np.zeros(500, dtype=int)
 
 
     if ((MAX_IMAGE_SET != "ALL")  and (imageCount > MAX_IMAGE_SET)):
-        imageCount = MAX_IMAGE_SET
+        imageCount = MAX_IMAGE_SET * 2 #Reading twice the files of Max allowed set so that post filtering we get required data set
         print(imageCount)
+
     baseFilenameList = [i.split('.')[0] for i in imageList[:imageCount]]
     filterImageList = []
 
     for i in range(imageCount):
         metaFilename = os.path.join(METADATA_DIR, '%s.json' % baseFilenameList[i])
         imageFilename = os.path.join(IMAGE_DIR, '%s.jpg' % baseFilenameList[i])
-        if (os.path.isfile(metaFilename) and os.path.isfile(imageFilename) == False) :
+        if (os.path.isfile(metaFilename) and os.path.isfile(imageFilename)) == False:
             print("Metadata File for Image %s does not exist", imageFilename)
 
-        metadata = json.loads(open(metaFilename).read())
+        f = open(metaFilename)
+        metadata = json.loads(f.read())
+        f.close()
         quantity = metadata['EXPECTED_QUANTITY']
         binItemData = metadata["BIN_FCSKU_DATA"]
         itemTypeCount = len(binItemData)
         filterImageList.append([baseFilenameList[i], quantity, itemTypeCount])
-        quantity_hist[quantity] = quantity_hist[quantity] + 1
-
-    print(len(filterImageList))
 
     image_df = pd.DataFrame(filterImageList, columns=["filename", "Quantity", "ItemTypeCount"])
+    image_df["Quantity"].hist(xlabelsize=6, ylabelsize=6)
+    image_df["ItemTypeCount"].hist(xlabelsize=6, ylabelsize=6)
 
-    uni_filenames = np.unique(image_df.filename.values)
-    print("The number of unique file names : {}".format(len(uni_filenames)))
     print("The distribution of the number of file as per Quanity for each image:")
     print(Counter(image_df.Quantity.values))
     print("Most Common Item Count Distribution")
     print(Counter(image_df.Quantity.values).most_common(5))
 
-    #plthist(quantity_hist)
-    #plthist(quantity_hist,10)
     return image_df
 
 def prepareTrainValidateTestSplitDataset(image_df, trainPercentage, validatePercentage, testPercentage):
     print("prepareTrainValidateTestSplitDataset -->")
-    print(ClassTestFileName)
 
     if ((1 - trainPercentage) != (validatePercentage + testPercentage)):
         print("Given Data Distribution is not correct")
@@ -81,78 +68,27 @@ def prepareTrainValidateTestSplitDataset(image_df, trainPercentage, validatePerc
 
     print("Image Count before Filtering ", image_df.shape[0])
     image_df = image_df[image_df['Quantity'] <= MAX_ITEM_COUNT]
-    print("Image Count After Filtering basd on Item Count", image_df.shape[0])
+    print("Image Count After Filtering based on Item Count", image_df.shape[0])
     image_df = image_df[image_df['ItemTypeCount'] <= MAX_ITEM_TYPE_COUNT]
-    print("Image Count After Filtering basd on Item Type Count", image_df.shape[0])
+    print("Image Count After Filtering based on Item Type Count", image_df.shape[0])
 
-    imageList = os.listdir(IMAGE_DIR)
-    imageCount = len(imageList)
+    imageCount = image_df.shape[0]
 
     if ((MAX_IMAGE_SET != "ALL")  and (imageCount > MAX_IMAGE_SET)):
         imageCount = MAX_IMAGE_SET
-        print(imageCount)
+        image_df = image_df.iloc[:imageCount]
+        print("Image Count After Image Limit Count check ", image_df.shape[0])
 
-    baseFilenameList = [i.split('.')[0] for i in imageList[:imageCount]]
-
-    listRandom = baseFilenameList.copy()
-    random.shuffle(listRandom)
-
-    # finding images that metadata exists
-    metaAvail = np.zeros(imageCount, dtype=bool)
-    for i in range(imageCount):
-        metaFilename = os.path.join(METADATA_DIR, '%s.json' % baseFilenameList[i])
-        imageFilename = os.path.join(IMAGE_DIR, '%s.jpg' % baseFilenameList[i])
-        if os.path.isfile(metaFilename) and os.path.isfile(imageFilename) :
-            # Filter Images with more than MAX_ITEM_COUNT
-            metadata = json.loads(open(metaFilename).read())
-            quantity = metadata['EXPECTED_QUANTITY']
-            if (quantity <= MAX_ITEM_COUNT):
-                metaAvail[i] = True
-            #else:
-                #print("Image %s is having % items in BIN, so skipping", baseFilenameList[i], quantity)
-
-
-
-    # assign validataion & test set
-    valSet = np.zeros(imageCount, dtype=bool)
-    valSetImageCount = int(round(imageCount * validatePercentage))
-
-    testSet = np.zeros(imageCount, dtype=bool)
-    testSetImageCount = int(round(imageCount * testPercentage))
-
-    count = 0
-    for i in range(imageCount):
-        idx = baseFilenameList.index(listRandom[i])
-        if metaAvail[idx]:
-            valSet[idx] = True
-            count = count + 1
-            if count == valSetImageCount:
-                break
-
-    count = 0
-    random.shuffle(listRandom)
-    for i in range(imageCount):
-        idx = baseFilenameList.index(listRandom[i])
-        if metaAvail[idx] and valSet[idx] != True:
-            testSet[idx] = True
-            count = count + 1
-            if count == testSetImageCount:
-                break
+    testDF, vaidationDF, trainDF = np.split(image_df,
+                                            [int(validatePercentage*len(image_df)),
+                                             int(testPercentage*len(image_df))])
 
     # writing out to textfile
     os.makedirs(INTERMEDIATE_DIS, exist_ok=True)
-    trainSetFilename = open(TRAIN_SET_FILENAME, 'w')
-    testSetFilename = open(TEST_SET_FILENAME, 'w')
-    validationSetFilename = open(VALIDATION_SET_FILENAME, 'w')
-    for i in range(imageCount):
-        if metaAvail[i]:
-            if valSet[i]:
-                validationSetFilename.write("%s\n" % (baseFilenameList[i]))
-            elif testSet[i]:
-                testSetFilename.write("%s\n" % (baseFilenameList[i]))
+    trainDF['filename'].to_csv(TRAIN_SET_FILENAME, sep=' ', index=False, header=False)
+    vaidationDF['filename'].to_csv(VALIDATION_SET_FILENAME, sep=' ', index=False, header=False)
+    testDF['filename'].to_csv(TEST_SET_FILENAME, sep=' ', index=False, header=False)
 
-            else:
-                trainSetFilename.write("%s\n" % (baseFilenameList[i]))
-    trainSetFilename.close()
-    testSetFilename.close()
-    validationSetFilename.close()
+    for i in range(len(ClassTestFileName)):
+        tempDF = testDF[testDF['Quantity'] == i]
+        tempDF['filename'].to_csv(ClassTestFileName[i], sep=' ', index=False, header=False)
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from DataLoadingUtils import TensorflowDataGenerator
 from common.ImageDataSetSplit import *
 from keras.callbacks import ModelCheckpoint
 from datetime import date
+from keras.utils.vis_utils import plot_model
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -29,20 +33,20 @@ model_dictionary = {m[0]:m[1] for m in inspect.getmembers(tf.keras.applications,
 model_names = model_dictionary.keys()
 
 parser = argparse.ArgumentParser(description='Image Object Counting Model')
-parser.add_argument('--model', '-m', metavar='MODEL', default='ResNet50', choices=model_names, help='model architecture: ' + ' | '.join(model_names) + ' (default: ResNet50)')
-parser.add_argument('-j', '--workers', default=8, type=int, metavar='N', help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=3, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--model', '-m', metavar='MODEL', default='VGG16', choices=model_names, help='model architecture: ' + ' | '.join(model_names) + ' (default: VGG16)')
+parser.add_argument('--epochs', default=5, type=int, metavar='N', help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch_size', default=128, type=int, metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('-b', '--batch_size', default=16, type=int, metavar='N', help='mini-batch size (default: 32)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, metavar='LR', help='initial learning rate')
 parser.add_argument('--lrd','--learning-rate-decay-step', default=10, type=int, metavar='N', help='learning rate decay epoch')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
-parser.add_argument('--max-target', default=20, type=int, metavar='N', help='maximum number of target images for validation (default: 20)')
+parser.add_argument('--max-target', default=20, type=int, metavar='N', help='smaximum number of target images for validation (default: 20)')
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=10, type=int, metavar='N', help='print frequency (default: 10)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('--evaluate', default=False, type=bool, metavar='BOOL', help='evaluate or train')
 parser.add_argument('--pd','--prepare-data', action='store_true', help='Prepare Data for Model Evaluation')
+parser.add_argument('-j', '--workers', default=12, type=int, metavar='N', help='number of data loading workers (default: 12)')
 
 def oneTimeImageProcssing():
     # Resize all images to 224x244, One time operation, so commenting call
@@ -52,9 +56,46 @@ def oneTimeImageProcssing():
     #convertRGB2HSVImages()
     return
 
+model_dictionary_preprocess = {m[0]:m[1] for m in inspect.getmembers(tf.keras.applications, inspect.ismodule)}
+
+def predictTestDataSet(modelname, model, filenameList):
+    datafile = open(filenameList, "r")
+    content_list = datafile.read().splitlines()
+
+    for filename in content_list:
+        metaFilename = os.path.join(METADATA_DIR, ('%s.json' % filename))
+        imageFilename = os.path.join(IMAGE_DIR, ('%s.jpg' % filename))
+
+        img = image.load_img(imageFilename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = model_dictionary_preprocess[modelname.lower()].preprocess_input(x)
+
+        metadata = json.loads(open(metaFilename).read())
+        actualQuantity = metadata['EXPECTED_QUANTITY']
+
+        predictedQuantity = model.predict(x)
+        predictedQuantity = np.argmax(predictedQuantity)
+        print("Actual:%d - Predicted:%d",format(actualQuantity),format(predictedQuantity))
+
+def plotModelEvalMetric(hist, metric):
+    # plotting training and validation metric
+    loss = hist.history[metric]
+    val_loss = hist.history['val_' + metric]
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, color='red', label='Training ' + metric )
+    plt.plot(epochs, val_loss, color='green', label='Validation' + metric)
+    plt.title('Training and Validation '+ metric)
+    plt.xlabel('Epochs')
+    plt.ylabel(metric)
+    plt.legend()
+    plt.show()
+
 def mainMenu():
     global args
+
     args = parser.parse_args()
+    print(args)
 
     image_df = dataVisualization()
 
@@ -67,7 +108,15 @@ def mainMenu():
     model_func = model_dictionary[args.model]
 
     print("Building Model")
-    model = model_func()
+    base_model = model_func(include_top=True,weights=None)
+    base_model.summary()
+
+    # Add a layer where input is the output of the  second last layer
+    x = Dense(8, activation='softmax', name='predictions')(base_model.layers[-2].output)
+
+    # Then create the corresponding model
+    my_model = Model(inputs=base_model.input, outputs=x)
+    my_model.summary()
 
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
         initial_learning_rate=args.lr,
@@ -78,33 +127,47 @@ def mainMenu():
     if args.resume:
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
-            model.load_weights(args.resume)
+            my_model.load_weights(args.resume)
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
             return
 
-    model.compile(loss='mse',optimizer=optimizer, metrics=['accuracy', 'mse'])
-
+    my_model.compile(loss='mse',optimizer=optimizer, metrics=['accuracy', 'mse'])
 
     # checkpoint
     callbacks_list = [
         tf.keras.callbacks.EarlyStopping(patience=2),
-        tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join("./",date.today().strftime("%B %d, %Y"),'model.{epoch:02d}-{mse:.2f}.h5'),monitor='mse', verbose=1, save_best_only=True, mode='max'),
+        tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join("./",date.today().strftime("%m-%d-%Y"),'model.{epoch:02d}-{mse:.2f}.h5'),
+            monitor='mse',
+            verbose=1,
+            save_best_only=True,
+            mode='max'),
         tf.keras.callbacks.TensorBoard(log_dir='./logs'),
     ]
 
     training_generator = TensorflowDataGenerator(IMAGE_DIR,TRAIN_SET_FILENAME,args.model,args.batch_size)
-    validation_generator = TensorflowDataGenerator(IMAGE_DIR,VALIDATION_SET_FILENAME,args.model,32)
+    validation_generator = TensorflowDataGenerator(IMAGE_DIR,VALIDATION_SET_FILENAME,args.model,args.batch_size)
 
-    model.fit(x=training_generator,
+    hist = my_model.fit(x=training_generator,
                 validation_data=validation_generator,
                 epochs=args.epochs,
                 verbose=1,
                 use_multiprocessing=True,
                 callbacks=callbacks_list,
                 initial_epoch= args.start_epoch,
-                workers=12)
+                workers=args.workers)
 
+    #plotModelEvalMetric(hist, 'loss')
+    #plotModelEvalMetric(hist, 'accuracy')
+    #plotModelEvalMetric(hist, 'mse')
+
+    #print('Final training loss \t', str(round((hist.history['loss'][-1]),2))+ ' %')
+    #print('Final Training accuracy ', str(round((hist.history['accuracy'][-1]*100),2))+ ' %')
+    #print('Final Validation loss \t', str(round((hist.history['val_loss'][-1]),2))+ ' %')
+    #print('Final Validation accuracy ', str(round((hist.history['val_accuracy'][-1]*100),2))+ '%')
+
+    predictTestDataSet(args.model, my_model, TEST_SET_FILENAME)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
